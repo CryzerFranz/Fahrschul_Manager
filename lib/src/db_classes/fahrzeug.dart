bf708980caf85ebb87ad4200797088b7cd79dc6e
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-04 12:35:43 +0100

    add new functions and do some changes to addFahrzeug

--- lib/src/db_classes/fahrzeug.dart
+++ lib/src/db_classes/fahrzeug.dart
@@ -1,151 +1,167 @@
 import 'package:fahrschul_manager/doc/intern/User.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-
 //TODO TEST
 /// Fügt ein Fahrzeug zur Fahrschule hinzu
-Future<void> addFahrzeug(String getriebe, String fahrzeugtyp, String marke, {String? label, bool anhaengerkupplung = false}) async{
-  try{
-    final fahrschuleObj = Benutzer().fahrschule;
-    if(fahrschuleObj == null)
-    {
-      throw Exception("Invalid User");
-    }
-    final getriebeObj = await getGetriebe(getriebe);
-    final fahrzeugtypObj = await getFahrzeugtyp(fahrzeugtyp);
-    final markeObj = await getMarke(marke);
-    if(getriebeObj == null || fahrzeugtypObj == null || markeObj == null)
-    {
-      throw Exception("Null values not allowed for: Getriebe ($getriebe); Fahrzeugtyp ($fahrzeugtyp); Marke ($marke)");
-    }
-    final user = Benutzer().parseUser;
-    if(user == null)
-    {
-      throw("no user");
-      //return;
-    }
-    
-    final fahrzeugObject = ParseObject("Fahrzeug")
-    ..set('Getriebe', getriebeObj)
-    ..set('Fahrzeugtyp', fahrzeugtypObj)
-    ..set('Marke', markeObj)
+Future<bool> addFahrzeug(
+    ParseObject getriebe, ParseObject fahrzeugtyp, ParseObject marke,
+    {String? label, bool anhaengerkupplung = false}) async {
+  final fahrschuleObj = Benutzer().fahrschule;
+  final user = Benutzer().parseUser;
+
+  if (fahrschuleObj == null || user == null) {
+    return false;
+  }
+
+  final fahrzeugObject = ParseObject("Fahrzeug")
+    ..set('Getriebe', getriebe)
+    ..set('Fahrzeugtyp', fahrzeugtyp)
+    ..set('Marke', marke)
     ..set('Label', label)
     ..set('Anhaengerkupplung', anhaengerkupplung)
     ..set('Fahrschule', fahrschuleObj);
 
   final ParseResponse response = await fahrzeugObject.save();
-  if(!response.success)
-  {
-    throw Exception(response.error?.message);
+  if (!response.success) {
+    return false;
   }
-  }catch(e)
-  {
-    throw(Exception("Error: addFahrzeug -> $e"));
-  }
-
+  return true;
 }
 
-/// Erhalte einen ParseObject von `Getriebe`.
-/// 
-/// ### Parameters:
-/// 
-/// - **`String` [typ]** : Bezeichnung des Getriebes.
-/// 
-/// ### Return value:
-/// - **[ParseObject?]** : `ParseObject` || `null`
-/// 
-/// ### Exception:
-/// - **[Exception]** 
-Future<ParseObject?> getGetriebe(final String typ) async
-{
-  try{
-    final QueryBuilder<ParseObject> parseQuery = QueryBuilder<ParseObject>(ParseObject('Getriebe'))
-    ..whereContains('Typ', typ);
+Future<List<ParseObject>> getAllGetriebe() async {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Getriebe'));
 
     final apiResponse = await parseQuery.query();
 
-    if (!apiResponse.success) 
-    {
+    if (!apiResponse.success || apiResponse.results == null) {
+      return [];
+    }
+
+    return apiResponse.results as List<ParseObject>;
+}
+
+Future<List<ParseObject>> getAllMarke() async {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Marke'));
+
+    final apiResponse = await parseQuery.query();
+
+    if (!apiResponse.success || apiResponse.results == null) {
+      return [];
+    }
+
+    return apiResponse.results as List<ParseObject>;
+}
+
+Future<List<ParseObject>> getAllFahrzeugtyp() async {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Fahrzeugtyp'));
+
+    final apiResponse = await parseQuery.query();
+
+    if (!apiResponse.success || apiResponse.results == null) {
+      return [];
+    }
+
+    return apiResponse.results as List<ParseObject>;
+}
+
+
+//TODO Wahrscheinlich danach nicht mehr nötig
+
+/// Erhalte einen ParseObject von `Getriebe`.
+///
+/// ### Parameters:
+///
+/// - **`String` [typ]** : Bezeichnung des Getriebes.
+///
+/// ### Return value:
+/// - **[ParseObject?]** : `ParseObject` || `null`
+///
+/// ### Exception:
+/// - **[Exception]**
+Future<ParseObject?> getGetriebeByTyp(final String typ) async {
+  try {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Getriebe'))
+          ..whereContains('Typ', typ);
+
+    final apiResponse = await parseQuery.query();
+
+    if (!apiResponse.success) {
       throw Exception(apiResponse.error?.message);
     }
-    if(apiResponse.results == null || apiResponse.results!.isEmpty)
-    {
+    if (apiResponse.results == null || apiResponse.results!.isEmpty) {
       return null;
     }
 
-  return apiResponse.results!.first as ParseObject;
-  }catch(e)
-  {
+    return apiResponse.results!.first as ParseObject;
+  } catch (e) {
     throw Exception("Error: getGetriebe -> $e");
   }
 }
 
 /// Erhalte einen ParseObject von `Marke`.
-/// 
+///
 /// ### Parameters:
-/// 
+///
 /// - **`String` [name]** : Name der Marke.
-/// 
+///
 /// ### Return value:
 /// - **[ParseObject?]** : `ParseObject` || `null`
-/// 
+///
 /// ### Exception:
-/// - **[Exception]** 
-Future<ParseObject?> getMarke(final String name) async
-{
-  try{
-    final QueryBuilder<ParseObject> parseQuery = QueryBuilder<ParseObject>(ParseObject('Marke'))
-    ..whereContains('Name', name);
+/// - **[Exception]**
+Future<ParseObject?> getMarkeByTyp(final String name) async {
+  try {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Marke'))
+          ..whereContains('Name', name);
 
     final apiResponse = await parseQuery.query();
 
-    if (!apiResponse.success) 
-    {
+    if (!apiResponse.success) {
       throw Exception(apiResponse.error?.message);
     }
-    if(apiResponse.results == null || apiResponse.results!.isEmpty)
-    {
+    if (apiResponse.results == null || apiResponse.results!.isEmpty) {
       return null;
     }
 
-  return apiResponse.results!.first as ParseObject;
-  } catch(e)
-  {
+    return apiResponse.results!.first as ParseObject;
+  } catch (e) {
     throw Exception("Error: getMarke -> $e");
   }
 }
 
 /// Erhalte einen ParseObject von `Fahrzeugtyp`.
-/// 
+///
 /// ### Parameters:
-/// 
+///
 /// - **`String` [typ]** : Bezeichnung des Fahrzeugtyps.
-/// 
+///
 /// ### Return value:
 /// - **[ParseObject?]** : `ParseObject` || `null`
-/// 
+///
 /// ### Exception:
-/// - **[Exception]** 
-Future<ParseObject?> getFahrzeugtyp(final String typ) async
-{
-  try{
-    final QueryBuilder<ParseObject> parseQuery = QueryBuilder<ParseObject>(ParseObject('Fahrzeugtyp'))
-    ..whereContains('Typ', typ);
+/// - **[Exception]**
+Future<ParseObject?> getFahrzeugtypByTyp(final String typ) async {
+  try {
+    final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject('Fahrzeugtyp'))
+          ..whereContains('Typ', typ);
 
     final apiResponse = await parseQuery.query();
 
-    if (!apiResponse.success) 
-    {
+    if (!apiResponse.success) {
       throw Exception(apiResponse.error?.message);
     }
-    if(apiResponse.results == null || apiResponse.results!.isEmpty)
-    {
+    if (apiResponse.results == null || apiResponse.results!.isEmpty) {
       return null;
     }
 
-  return apiResponse.results!.first as ParseObject;
-  }catch(e)
-  {
+    return apiResponse.results!.first as ParseObject;
+  } catch (e) {
     throw Exception("Error: getFahrzeugtyp -> $e");
   }
 }
