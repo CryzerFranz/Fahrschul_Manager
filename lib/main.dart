9b5da6e1f9c51fd91ad34e3dd1e159a73ee96753
z0r0k <lolofleagends@web.de>
2024-11-08 12:20:41 +0100

    Changes
    Änderungen angepasst

--- lib/main.dart
+++ lib/main.dart
@@ -30,54 +30,11 @@ void main() async {
       clientKey: _clientKey,
       debug: true,
     );
-  //ParseUser? currentUser = await ParseUser("test@gmail","12345678" , "test@gmail");
-  //await currentUser.login();
+  Benutzer().initialize();
 
- //final uquery = QueryBuilder<ParseUser>(ParseUser.forQuery())
- //     ..whereEqualTo('objectId', currentUser.objectId!);
-
-  //  final ParseResponse response = await uquery.query();
-  // ParseUser User = response.results!.first as ParseUser;
-
-
-
-  //final QueryBuilder<ParseObject> query = QueryBuilder<ParseObject>(ParseObject('_Role'))
-  // ..whereRelatedTo('users', '_User', currentUser.objectId);
-
-  // final QueryBuilder<ParseObject> roleQuery = QueryBuilder<ParseObject>(ParseObject('_Role'))
-  //     ..whereContainedIn('users', [User]);
-
-
-  //final QueryBuilder<ParseObject> query =
-  //        QueryBuilder<ParseObject>(ParseObject('_Role'))           
-  //          ..whereContains('name', 'Fahrlehrer');
-  // final ParseResponse roleResp = await roleQuery.query();
-  // for (var role in roleResp.results!) {
-  //       print('Role: ${(role as ParseObject).get<String>('name')}');
-  //     }
-
-  ParseUser? currentUser = await ParseUser("test@gmail","12345678" , "test@gmail");
-  ParseUser? user = await ParseUser.currentUser() as ParseUser?;
-  bool is342 = await Benutzer().initialize(user: currentUser);
-  bool nowLooged = await Benutzer().login();
   
 
-  //final user = ParseUser("pp@gmail.com", "12345678", "pp@gmail.com");
-  //final ort = await getOrt("Stuttgart", "70619");
-  //if(ort != null) {
-  //  await fahrschuleRegistration("Luis die 2", ort, "Nuenberg", "A12312", "luis@gmail.com", "ABC12345", "vorname", "name");
-  //}
 
-    //final f = await getFahrschuleWithId("6oyNDpad2A");
-    //await createFahrlehrer("rolen", "tester", "test@gmail", f!, "12345678");
-    //await addFahrstunde(DateTime.utc(2024,12,12), DateTime.utc(2024,12,12,15,15,0), dummyRenault);
-    //final status = await getStatus("Nicht zugewiesen");
-    //print(status?.get('Typ'));
-    
-    //final user = await ParseUser.currentUser() as ParseUser?;
-    //final islog = await logout(user!);
-    //final f = await getFahrschuleWithId("6oyNDpad2A");
-    //final test = await createFahrschueler("test", "login", "pp@gmail.com", "12345678", f!);
     
     runApp(MyApp());
   } else {
@@ -85,76 +42,7 @@ void main() async {
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return CalendarControllerProvider(
-      controller: EventController(),
-      child: Scaffold(body: Container(height: 700, child: WeekView())),
-    );
-    // return const Scaffold(body:
-    // MonthView());
-  }
-}
-
-class UserPage extends StatelessWidget {
-  ParseUser? currentUser;
-
-  @override
-  Widget build(BuildContext context) {
-    void doUserLogout() async {
-      var response = await currentUser!.logout();
-      if (response.success) {
-        print("sucess LOGOUT");
-      } else {
-        print("ERROR LOGOUT");
-      }
-    }
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('User logged in - Current User'),
-        ),
-        body: FutureBuilder<ParseUser?>(
-            future: getCurrentUser(),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                case ConnectionState.waiting:
-                  return Center(
-                    child: Container(
-                        width: 100,
-                        height: 100,
-                        child: CircularProgressIndicator()),
-                  );
-                default:
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Center(
-                            child: Text('Hello, ${snapshot.data!.username}')),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Container(
-                          height: 50,
-                          child: ElevatedButton(
-                            child: const Text('Logout'),
-                            onPressed: () => doUserLogout(),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-              }
-            }));
-  }
-}
 
 class ErrorApp extends StatelessWidget {
   @override
@@ -173,41 +61,6 @@ class ErrorApp extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      title: 'Flutter - Parse Server',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: FutureBuilder<bool>(
-          future: hasUserLogged(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return Scaffold(
-                  body: Center(
-                    child: Container(
-                        width: 100,
-                        height: 100,
-                        child: CircularProgressIndicator()),
-                  ),
-                );
-              default:
-                if (snapshot.hasData && snapshot.data!) {
-                  return UserPage(); // HOMEPAGE
-                } else {
-                  return MyWidget(); // LOGIN-PAGE
-                }
-            }
-          }),
-    );
-  }
-}
 
 Future<String?> getApplicationID() async {
   const platform = MethodChannel('com.example.fahrschul_manager/keys');
@@ -231,19 +84,6 @@ Future<String?> getClientID() async {
   }
 }
 
-class ErrorApp extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       title: 'Internal Error',
-       theme: ThemeData(
-         primarySwatch: Colors.red,
-         visualDensity: VisualDensity.adaptivePlatformDensity,
-       ),
-       home:const Scaffold(body: Center(child: Text("!Internal Error!", style: TextStyle(color: Colors.red)))),
-     );
-   }
- }
 
 class MyApp extends StatelessWidget {
   @override
@@ -256,7 +96,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: FutureBuilder<bool>(
-          future: hasUserLogged(),
+          future: Benutzer().hasUserLogged(),
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.none:
