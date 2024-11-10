8fe16e8743100d691dfa81683d57bb541edd270e
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-10 16:13:37 +0100

    add function to create new DB entry for Fahrstunde

--- lib/src/db_classes/fahrstunde.dart
+++ lib/src/db_classes/fahrstunde.dart
@@ -1,4 +1,3 @@
-import 'package:fahrschul_manager/doc/intern/Dummys.dart';
 import 'package:fahrschul_manager/doc/intern/User.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
@@ -6,50 +5,48 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 Future<void> addFahrstunde(
   DateTime datum,
   DateTime zeit,
-  ParseObject fahrzeug, {
+  String titel, {
+  ParseObject? fahrzeug,
   DateTime? endDatum,
   DateTime? endZeit,
   DateTime? pufferZeit,
   ParseObject? fahrschueler,
-  String? titel,
   String? beschreibung,
 }) async {
-  try {
-    //TODO brauche fahrlehrer ParseObject nicht ParseUser (von ParseUser ID suchen in Fahrlehrer, sicher gehen das parseUser Fahrlehrer ist)
-    if(!Benutzer().isFahrlehrer!)
-    {
-      throw("No permission");
+    if (!Benutzer().isFahrlehrer!) {
+      throw ("No permission");
     }
 
-    //final fahrlehrer = await getLocalStorageUser();
-    //final fahrlehrer = await getFahrlehrerByUserId("hCHGkgyRs9"); //TODO ist nur test
-    
     DateTime dbDate =
         datum.add(Duration(hours: zeit.hour, minutes: zeit.minute));
-    DateTime? dbEndDate;
+
+    final termin = ParseObject("Fahrstunden")
+      ..set('Fahrlehrer', Benutzer().dbUser)
+      ..set('Titel', titel)
+      ..set("Datum", dbDate);
+
+    if (fahrzeug != null) {
+      termin.set("Fahrzeug", fahrzeug);
+    }
+    if (beschreibung != null) {
+      termin.set("Beschreibung", beschreibung);
+    }
 
     if (endDatum != null) {
-      dbEndDate =
+      DateTime dbEndDate =
           endDatum.add(Duration(hours: endZeit!.hour, minutes: endZeit.minute));
       if (pufferZeit != null) {
         dbEndDate = dbEndDate.add(Duration(minutes: pufferZeit.minute));
       }
+      termin.set("EndDatum", dbEndDate);
     }
 
-    final termin = ParseObject("Fahrstunden")
-      ..set('Fahrlehrer', Benutzer().dbUser) //TODO DB ParseObject nicht lokales einfügen
-      //..set('Fahrschueler', fahrschueler)
-      ..set('Fahrzeug', fahrzeug)
-      ..set('Datum', dbDate);
-      // ..set('EndDatum', null)
-      // ..set('Titel', null)
-      // ..set('Beschreibung', null);
+    if (fahrschueler != null) {
+      termin.set('Fahrschueler', fahrschueler);
+    }
 
     final ParseResponse response = await termin.save();
     if (!response.success) {
       throw Exception(response.error?.message);
     }
-  } catch (e) {
-    throw Exception("Error: addFahrstunde -> $e");
-  }
 }
