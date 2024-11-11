95f3ccd5e7abd9300f1668c26c96c1a1bbc97aec
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-11 17:56:20 +0100

    function to create fahrstunde and function to retrieve all fahrstunden for logged in user

--- lib/src/db_classes/fahrstunde.dart
+++ lib/src/db_classes/fahrstunde.dart
@@ -1,52 +1,96 @@
+import 'package:calendar_view/calendar_view.dart';
 import 'package:fahrschul_manager/doc/intern/User.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-//TODO TEST
+//TODO !!FRONTEND sollte überprüfen das enddatum nicht kleiner als datum ist.!!
 Future<void> addFahrstunde(
   DateTime datum,
-  DateTime zeit,
+  DateTime endDatum,
   String titel, {
   ParseObject? fahrzeug,
-  DateTime? endDatum,
-  DateTime? endZeit,
   DateTime? pufferZeit,
   ParseObject? fahrschueler,
   String? beschreibung,
 }) async {
-    if (!Benutzer().isFahrlehrer!) {
-      throw ("No permission");
-    }
+  if (!Benutzer().isFahrlehrer!) {
+    throw ("No permission");
+  }
+  // Event in die Datenbank abspeichern
+  //DateTime dbDate = datum.add(Duration(hours: zeit.hour, minutes: zeit.minute));
 
-    DateTime dbDate =
-        datum.add(Duration(hours: zeit.hour, minutes: zeit.minute));
+  final termin = ParseObject("Fahrstunden")
+    ..set('Fahrlehrer', Benutzer().dbUser)
+    ..set('Titel', titel)
+    ..set("Datum", datum);
 
-    final termin = ParseObject("Fahrstunden")
-      ..set('Fahrlehrer', Benutzer().dbUser)
-      ..set('Titel', titel)
-      ..set("Datum", dbDate);
+  if (fahrzeug != null) {
+    termin.set("Fahrzeug", fahrzeug);
+  }
+  if (beschreibung != null) {
+    termin.set("Beschreibung", beschreibung);
+  }
 
-    if (fahrzeug != null) {
-      termin.set("Fahrzeug", fahrzeug);
+  DateTime dbEndDate = endDatum;
+    if (pufferZeit != null) {
+      dbEndDate = endDatum.add(Duration(minutes: pufferZeit.minute));
     }
-    if (beschreibung != null) {
-      termin.set("Beschreibung", beschreibung);
-    }
+    termin.set("EndDatum", dbEndDate);
 
-    if (endDatum != null) {
-      DateTime dbEndDate =
-          endDatum.add(Duration(hours: endZeit!.hour, minutes: endZeit.minute));
-      if (pufferZeit != null) {
-        dbEndDate = dbEndDate.add(Duration(minutes: pufferZeit.minute));
-      }
-      termin.set("EndDatum", dbEndDate);
-    }
+  if (fahrschueler != null) {
+    termin.set('Fahrschueler', fahrschueler);
+  }
 
-    if (fahrschueler != null) {
-      termin.set('Fahrschueler', fahrschueler);
-    }
+  final ParseResponse response = await termin.save();
+  if (!response.success) {
+    throw Exception(response.error?.message);
+  }
 
-    final ParseResponse response = await termin.save();
-    if (!response.success) {
-      throw Exception(response.error?.message);
-    }
+  // Event erstellen für calender_view package
+}
+
+Future<List<CalendarEventData>> getUserFahrstunden() async {
+  List<CalendarEventData> events = [];
+  String role = Benutzer().isFahrlehrer! ? "Fahrlehrer" : "Fahrschueler";
+  final QueryBuilder<ParseObject> parseQuery =
+      QueryBuilder<ParseObject>(ParseObject('Fahrstunden'))
+        ..whereGreaterThan("Datum", DateTime.now())
+        ..whereContains(role, Benutzer().dbUser!.objectId!);
+
+  final apiResponse = await parseQuery.query();
+  if (!apiResponse.success) {
+    throw ("API Response failed. Check Network connection.");
+  }
+
+  if (apiResponse.results == null) {
+    return [];
+  }
+
+  for (var result in apiResponse.results!) {
+    CalendarEventData event;
+    DateTime? endDate = result.get<DateTime?>("EndDatum") as DateTime?;
+
+    event = CalendarEventData(
+        title: result.get<String>("Titel"),
+        date: result.get<DateTime>("Datum"),
+        description: result.get<String?>("Beschreibung"),
+        endDate: endDate,
+        startTime: DateTime(
+          result.get<DateTime>("Datum").year,
+          result.get<DateTime>("Datum").month,
+          result.get<DateTime>("Datum").day,
+          result.get<DateTime>("Datum").hour,
+          result.get<DateTime>("Datum").minute,
+        ),
+         endTime: DateTime(
+          result.get<DateTime>("EndDatum").year,
+          result.get<DateTime>("EndDatum").month,
+          result.get<DateTime>("EndDatum").day,
+          result.get<DateTime>("EndDatum").hour,
+          result.get<DateTime>("EndDatum").minute,
+        )
+    );
+
+    events.add(event);
+  }
+  return events;
 }
