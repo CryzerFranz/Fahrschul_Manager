9f9082a2364d66cce7370504185f76ae34697030
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-18 19:37:38 +0100

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color navBarBackgroundColor = Color.fromARGB(255, 59, 59, 59);
+const Color navBarSelectedColor = Colors.white;
+const Color navBarUnSelectedColor = Color.fromARGB(255, 161, 161, 161);
