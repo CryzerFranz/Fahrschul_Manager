2fd2656f721911d17433eafd86e1690e30cbde5b
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-19 14:48:56 +0100

    add main colors to constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const Color navBarBackgroundColor = Color.fromARGB(255, 59, 59, 59);
 const Color navBarSelectedColor = Colors.white;
 const Color navBarUnSelectedColor = Color.fromARGB(255, 161, 161, 161);
+const Color mainColor = Color(0xFF00BF6D);
+const Color mainColorComplementaryFirst = Color(0xFF87CEEB);
+const Color mainColorComplementarySecond = Color(0xFFFFD700);
+
