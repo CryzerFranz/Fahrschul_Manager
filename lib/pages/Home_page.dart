e7852be12778b142a1206f0635961b93cab8e848
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-19 15:36:02 +0100

    update navbar

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,41 +1,17 @@
-import 'dart:ui';
 
 import 'package:fahrschul_manager/widgets/navBar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Selected Index: $_selectedIndex'),
+        child: Text('Hallo'),
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.1), // Transparent background
-          borderRadius: BorderRadius.circular(20), // Rounded corners
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 10,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: CustomNavBar()));
+      bottomNavigationBar: CustomNavBar());
+  }
 }
-}
-
--- lib/widgets/navBar.dart
+++ lib/widgets/navBar.dart
@@ -7,48 +7,40 @@ class CustomNavBar extends StatefulWidget {
 }
 
 class _CustomNavBarState extends State<CustomNavBar> {
-  int selectedIndex = 0; // Track the selected icon's index
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Other content of the page
-          
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 35),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: 70,
-                decoration: BoxDecoration(
-                  color: navBarBackgroundColor.withOpacity(0.6),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: 2,
-                      blurRadius: 10,
-                      offset: Offset(0, 20),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buildNavBarIcon(Icons.chat_bubble_outline, 0),
-                    buildNavBarIcon(Icons.search, 1),
-                    buildNavBarIcon(Icons.timer, 2),
-                    buildNavBarIcon(Icons.notifications, 3),
-                    buildNavBarIcon(Icons.person_outline, 4),
-                  ],
-                ),
-              ),
+    return Padding(
+      padding: EdgeInsets.only(
+        left: MediaQuery.of(context).size.width * 0.05, // 5% padding on left
+        right: MediaQuery.of(context).size.width * 0.05, // 5% padding on right
+        bottom: 40.0, // 10px bottom padding (adjust as needed)
+      ), // 5% padding on both sides
+      child: Container(
+        height: 66,
+        decoration: BoxDecoration(
+          color: navBarBackgroundColor.withOpacity(0.6),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, 20),
             ),
-          ),
-        ],
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildNavBarIcon(Icons.chat_bubble_outline, 0),
+            buildNavBarIcon(Icons.search, 1),
+            buildNavBarIcon(Icons.timer, 2),
+            buildNavBarIcon(Icons.notifications, 3),
+            buildNavBarIcon(Icons.person_outline, 4),
+          ],
+        ),
       ),
     );
   }
@@ -58,11 +50,10 @@ class _CustomNavBarState extends State<CustomNavBar> {
       icon: Icon(
         icon,
         color: selectedIndex == index ? navBarSelectedColor : navBarUnSelectedColor,
-        // Lighter color for selected, darker for unselected
       ),
       onPressed: () {
         setState(() {
-          selectedIndex = index; // Update the selected index
+          selectedIndex = index;
         });
       },
     );
