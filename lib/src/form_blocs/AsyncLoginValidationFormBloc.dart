f35db7dc565792fbc1a4b2f3c304719dfbca1f79
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-20 19:16:43 +0100

    Create AsyncLoginValidationFormBloc.dart

---
+++ lib/src/form_blocs/AsyncLoginValidationFormBloc.dart
@@ -0,0 +1,66 @@
+import 'package:fahrschul_manager/src/db_classes/fahrschule.dart';
+import 'package:fahrschul_manager/src/db_classes/user.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+
+/// Eine FormBloc klasse zur asynchronen Validierung einer Form.
+/// Diese gilt für die erste Seite der Registrierung
+class AsyncLoginValidationFormBloc extends FormBloc<String, String> {
+  // ------------------------Widget list -------------------------
+  final emailTFFormBloc = TextFieldBloc(
+    validators: [
+      (String? value) {
+        if (value == null || value.isEmpty) {
+          return 'Bitte geben Sie eine E-Mail Addresse ein.';
+        }
+        return null;
+      },
+    ],
+    asyncValidatorDebounceTime: const Duration(milliseconds: 300),
+  );
+
+  final passwordTFFormBloc = TextFieldBloc(
+    validators: [
+      (String? value) {
+        if (value == null || value.isEmpty) {
+          return 'Bitte geben Sie ein Passwort ein.';
+        }
+        return null;
+      },
+    ],
+  );
+
+  //------------------------------------------------------------------
+  @override
+  Future<void> onSubmitting() async {
+    try {
+      //validieren
+      emailTFFormBloc.validate();
+      passwordTFFormBloc.validate();
+
+      bool isValid = await Benutzer()
+          .login(emailTFFormBloc.value.trim(), passwordTFFormBloc.value.trim());
+      if(isValid)
+      {
+        emitSuccess();
+      }
+      else{
+        emailTFFormBloc.addFieldError("Angegebene Daten sind Falsch.");
+        passwordTFFormBloc.addFieldError("Angegebene Daten sind Falsch.");
+        emitFailure(failureResponse: "Falsche Daten");
+      }
+    } catch (e) {
+      emitFailure(failureResponse: "Netzwerk fehler");
+    }
+  }
+
+  /// Konstruktor
+  AsyncLoginValidationFormBloc() {
+    addFieldBlocs(fieldBlocs: [emailTFFormBloc, passwordTFFormBloc]);
+
+    /// Asynchrone Validierung hinzufügen
+    //fahrschulnameBloc.addAsyncValidators(
+    //  [validationFahrschulName],
+    //);
+  }
+}
