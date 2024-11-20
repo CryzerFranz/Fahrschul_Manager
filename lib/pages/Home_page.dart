8353883eecf8d8a64896f4e4aa0a989c971d2e4b
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-20 11:57:48 +0100

    Update Home_page.dart

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,7 +1,7 @@
-
+import 'package:fahrschul_manager/pages/authentication/login_page.dart';
+import 'package:fahrschul_manager/widgets/3dCard.dart';
 import 'package:fahrschul_manager/widgets/navBar.dart';
 import 'package:flutter/material.dart';
-
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,9 +9,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hallo'),
-      ),
-      bottomNavigationBar: CustomNavBar());
+      // extendBody: true -> damit bottomnavbar transparent sein kann
+      extendBody: true,
+        body: SingleChildScrollView(
+              padding: const EdgeInsets.only(top: 100, left: 20, right: 20 ),
+              child: Column(
+                children: [
+                  Custom3DCard(
+                    title: 'Test 3D Card mit widget',
+                    widget: CustomNavBar(),
+                  ),
+                  const SizedBox(height: 30),
+                  Custom3DCard(title: "Meine Fahrschüler", widget: Text("52")),
+                 
+                ],
+              ),
+            ),
+        bottomNavigationBar: CustomNavBar());
   }
 }
+
