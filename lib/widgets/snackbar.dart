66c8197ec3dd11ac6a8eab67e50c4031095749c1
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-20 19:17:40 +0100

    remove Materialbanner. Return SnackBar

--- lib/widgets/snackbar.dart
+++ lib/widgets/snackbar.dart
@@ -2,12 +2,12 @@
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 
-MaterialBanner showErrorSnackbar(String message, String title) {
-  return MaterialBanner(
+SnackBar showErrorSnackbar(String message, String title) {
+  return SnackBar(
                   /// need to set following properties for best effect of awesome_snackbar_content
                   elevation: 0,
+                  behavior: SnackBarBehavior.floating,
                   backgroundColor: Colors.transparent,
-                  forceActionsBelow: true,
                   content: AwesomeSnackbarContent(
                     title: title,
                     message:
@@ -15,9 +15,6 @@ MaterialBanner showErrorSnackbar(String message, String title) {
 
                     /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
                     contentType: ContentType.failure,
-                    // to configure for material banner
-                    inMaterialBanner: true,
                   ),
-                  actions: const [SizedBox.shrink()],
                 );
 }
