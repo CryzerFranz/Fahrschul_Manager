27781e355e469a0e0c9317bc68c18fa03c049401
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-24 17:18:09 +0100

    add loadingScreen function that return a widget to display loading indicator

--- lib/widgets/loadingIndicator.dart
+++ lib/widgets/loadingIndicator.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:fahrschul_manager/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 
 LoadingIndicator pacmanLoadingIndicator()
@@ -9,4 +10,16 @@ LoadingIndicator pacmanLoadingIndicator()
                           indicatorType: Indicator.pacman,
                           colors: [mainColor, mainColorComplementaryFirst, mainColorComplementarySecond]
                         );
+}
+
+Widget loadingScreen()
+{
+    return Scaffold(
+                  body: Center(
+                    child: Container(
+                        width: 100,
+                        height: 100,
+                        child: pacmanLoadingIndicator()),
+                  ),
+                );
 }
