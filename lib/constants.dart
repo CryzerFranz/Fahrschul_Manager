55aef49985a005b48d3ca905b478a50859af8474
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-28 12:56:57 +0100

    add mainColorShade100 , mainColorShade300

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 const Color navBarBackgroundColor = Color.fromARGB(255, 59, 59, 59);
 const Color navBarSelectedColor = Colors.white;
 const Color navBarUnSelectedColor = Color.fromARGB(255, 161, 161, 161);
+
+// Main color
 const Color mainColor = Color(0xFF00BF6D);
+
+const Color mainColorShade100 = Color.fromARGB(255, 178, 255, 218);
+const Color mainColorShade300 = Color.fromARGB(255, 128, 222, 154);
+
 const Color mainColorComplementaryFirst = Color(0xFF87CEEB);
 const Color mainColorComplementarySecond = Color(0xFFFFD700);
 
