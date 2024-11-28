c8181fbe1ba8ac63b72629731f8653f2e3600c12
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-28 23:45:20 +0100

    add const contructor

--- lib/pages/fahrschueler_liste/fahrschueler_liste_page.dart
+++ lib/pages/fahrschueler_liste/fahrschueler_liste_page.dart
@@ -10,6 +10,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FahrschuelerListePage extends StatelessWidget {
+  const FahrschuelerListePage({
+    super.key,
+  });
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
