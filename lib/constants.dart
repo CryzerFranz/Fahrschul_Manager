4ba9f6d4768d8f666696505a62a5a08758ab4ef2
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-11-29 11:57:44 +0100

    add new colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
+// NavBar Colors
 const Color navBarBackgroundColor = Color.fromARGB(255, 59, 59, 59);
 const Color navBarSelectedColor = Colors.white;
 const Color navBarUnSelectedColor = Color.fromARGB(255, 161, 161, 161);
 
-// Main color
-const Color mainColor = Color(0xFF00BF6D);
+// TabBar Colors
+const Color tabBarOrangeShade100 = Color.fromARGB(255, 255, 224, 178); 
+const Color tabBarOrangeShade300 = Color.fromARGB(255, 255, 183, 77); 
 
-const Color mainColorShade100 = Color.fromARGB(255, 178, 255, 218);
-const Color mainColorShade300 = Color.fromARGB(255, 128, 222, 154);
+const Color tabBarRedShade100 = Color.fromARGB(255, 255, 205, 210); 
+const Color tabBarRedShade300 = Color.fromARGB(255, 229, 115, 115);
+
+const Color tabBarMainColorShade100 = Color.fromARGB(255, 178, 255, 218);
+const Color tabBarMainColorShade300 = Color.fromARGB(255, 128, 222, 154);
+
+// Main Colors
+const Color mainColor = Color(0xFF00BF6D);
 
 const Color mainColorComplementaryFirst = Color(0xFF87CEEB);
 const Color mainColorComplementarySecond = Color(0xFFFFD700);
