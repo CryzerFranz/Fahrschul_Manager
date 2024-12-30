605482bbf4c081d53e59d668b9b33d7d7409481b
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-12-30 09:21:11 +0100

    change page indicator

--- lib/pages/home/homepage_fahrlehrer.dart
+++ lib/pages/home/homepage_fahrlehrer.dart
@@ -9,6 +9,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomePageFahrlehrer extends StatelessWidget {
   const HomePageFahrlehrer({super.key});
@@ -60,36 +61,50 @@ class HomePageFahrlehrer extends StatelessWidget {
               ),
               SizedBox(height: 30),
               Custom3DCard(
-  title: "Dein nächster Termin",
-  colors: const [
-               mainColor,
-               mainColor,
-               tabBarMainColorShade100],
-  widget: LayoutBuilder(
-    builder: (context, constraints) {
-      return Container(
-        height: 150,
-        color: Colors.transparent,
-        child: PageView.builder(
-          scrollDirection: Axis.horizontal, // Enables horizontal scrolling
-          itemCount: state.appointments.length, // Dynamic length based on the list
-          itemBuilder: (context, index) {
-            return Container(
-              width: constraints.maxWidth, // Full width of Custom3DCard
-              //margin: const EdgeInsets.symmetric(horizontal: 8.0), // Optional spacing
-              // decoration: BoxDecoration(
-              //   color: Colors.blueAccent, // Background color for visibility
-              //   borderRadius: BorderRadius.circular(8), // Optional rounded corners
-              // ),
-              alignment: Alignment.center,
-              child: nextFahrstundeContent(state.appointments[index], index, state.appointments.length)
-            );
-          },
-        ),
-      );
-    },
-  ),
-)
+                title: "Dein nächster Termin",
+                colors: const [mainColor, mainColor, tabBarMainColorShade100],
+                widget: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final pageController = PageController();
+
+                    return Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          color: Colors.transparent,
+                          child: PageView.builder(
+                            controller: pageController,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: state.appointments.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                  width: constraints.maxWidth,
+                                  alignment: Alignment.center,
+                                  child: nextFahrstundeContent(
+                                      state.appointments[index],
+                                      index,
+                                      state.appointments.length));
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        SmoothPageIndicator(
+                          controller: pageController,
+                          count: state.appointments.length,
+                          effect: const JumpingDotEffect(
+                            verticalOffset: 16,
+                            jumpScale: 1.5,
+                            dotHeight: 12,
+                            dotWidth: 12,
+                            activeDotColor: mainColorComplementarySecond,
+                            dotColor: mainColorComplementaryFirstShade100,
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              )
               // Container(
               // height: 172,
               // color: Colors.transparent,
@@ -140,10 +155,14 @@ class HomePageFahrlehrer extends StatelessWidget {
             Expanded(
               child: Row(
                 children: [
-                  Icon( fahrschuelerText == "-" ? 
-                    FontAwesomeIcons.userXmark : FontAwesomeIcons.user ,
+                  Icon(
+                    fahrschuelerText == "-"
+                        ? FontAwesomeIcons.userXmark
+                        : FontAwesomeIcons.user,
                     size: 18,
-                    color: fahrschuelerText == "-" ? tabBarRedShade300 : Colors.white,
+                    color: fahrschuelerText == "-"
+                        ? tabBarRedShade300
+                        : Colors.white,
                   ),
                   const SizedBox(width: 8),
                   Column(
@@ -167,10 +186,13 @@ class HomePageFahrlehrer extends StatelessWidget {
             Expanded(
               child: Row(
                 children: [
-                  Icon( fahrzeugText == "-" ?
-                     Icons.car_crash : FontAwesomeIcons.car,
+                  Icon(
+                    fahrzeugText == "-"
+                        ? Icons.car_crash
+                        : FontAwesomeIcons.car,
                     size: fahrzeugText == "-" ? 24 : 18,
-                    color: fahrzeugText == "-" ? tabBarRedShade300 : Colors.white,
+                    color:
+                        fahrzeugText == "-" ? tabBarRedShade300 : Colors.white,
                   ),
                   const SizedBox(width: 8),
                   Column(
@@ -193,14 +215,19 @@ class HomePageFahrlehrer extends StatelessWidget {
           ],
         ),
         const SizedBox(height: 10),
-        Center(child: Column(children:[
+        Center(
+            child: Column(children: [
           const Icon(Icons.access_time_outlined, size: 20, color: Colors.white),
-        const SizedBox(height: 3),
-          Text(next.getDateRange(), style: const TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w600),),
+          const SizedBox(height: 3),
+          Text(
+            next.getDateRange(),
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+          ),
           const SizedBox(height: 5),
-          Text(next.getTimeRange(), style: const TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w600),),
+          Text(
+            next.getTimeRange(),
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+          ),
         ])),
         //Row(
         //  children: [
@@ -232,7 +259,9 @@ class HomePageFahrlehrer extends StatelessWidget {
         //  ],
         //),
         const SizedBox(height: 5),
-        Center(child:Text("${++currentIndex} von $length", style: const TextStyle(
+        Center(
+            child: Text("${++currentIndex} von $length",
+                style: const TextStyle(
                   color: Colors.white,
                   fontSize: 14,
                   fontWeight: FontWeight.bold,
