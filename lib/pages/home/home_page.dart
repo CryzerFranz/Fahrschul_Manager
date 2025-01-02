600c8c2d1874e35f0920f361c715b0ac26c00318
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2025-01-02 11:40:00 +0100

    fix out of range error by loggin out from fahrlehrer account to fahrschüler account

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,34 +12,59 @@ import '../calendar_page/calendar_page.dart';
 import '../fahrschueler_liste/fahrschueler_liste_page.dart';
 import '../profil_page/profil_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  final List<Widget> _pagesFahrlehrer = const [
-    FahrschuelerListePage(),
-    CalendarPage(),
-    HomePageFahrlehrer(),
-    FahrschulePage(),
-    ProfilPage(),
-  ];
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-  final List<Widget> _pagesFahrschueler = const [
-    CalendarPage(),
-    HomePageFahrschueler(),
-    FahrschulePage(),
-    ProfilPage(),
-  ];
+class _HomePageState extends State<HomePage> {
+  late final NavBarBloc navBarBloc;
+  late final bool isFahrlehrer;
+  late final List<Widget> assignedPages;
+
+  @override
+  void initState() {
+    super.initState();
+
+    navBarBloc = context.read<NavBarBloc>();
+
+    isFahrlehrer = Benutzer().isFahrlehrer ?? false;
+
+    // Initialize role in NavBarBloc
+    navBarBloc.add(NavBarRoleInitialized(isFahrlehrer));
+
+    // Assign pages based on role
+    assignedPages = isFahrlehrer
+        ? const [
+            FahrschuelerListePage(),
+            CalendarPage(),
+            HomePageFahrlehrer(),
+            FahrschulePage(),
+            ProfilPage(),
+          ]
+        : const [
+            CalendarPage(),
+            HomePageFahrschueler(),
+            FahrschulePage(),
+            ProfilPage(),
+          ];
+
+    // Set initial selected page
+    navBarBloc.add(
+      isFahrlehrer ? const NavBarItemTapped(2) : const NavBarItemTapped(1),
+    );
+  }
+
+  @override
+  void dispose() {
+    navBarBloc.add(NavBarItemTapped(0));
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final isFahrlehrer = Benutzer().isFahrlehrer ?? false;
-
-    // Initialize role in NavBarBloc
-    context.read<NavBarBloc>().add(NavBarRoleInitialized(isFahrlehrer));
-
-    // Select pages based on role
-    final assignedPages = isFahrlehrer ? _pagesFahrlehrer : _pagesFahrschueler;
-
     return Scaffold(
       extendBody: true,
       body: BlocBuilder<NavBarBloc, NavBarState>(
@@ -51,6 +76,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
-
